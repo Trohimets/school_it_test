ed6c31ee4a46a8eb37ec6ff8f085c0a70b10d904
Константин Трохимец <katrohimets@mail.ru>
2023-01-30 15:22:46 +0300

    add structure of project

--- api.py
+++ api.py
@@ -12,12 +12,9 @@ class Usergender(BaseModel):
     gender: str
     limit: int
 
+
 class Useradd(BaseModel):
     id: int
-    name: str
-    gender: str
-
-class Updateuser(BaseModel):
     name: str
     gender: str
 
@@ -26,54 +23,27 @@ class Deleteuser(BaseModel):
     name: str
 
 
-
 app = FastAPI()
 
-
-
-
-@app.get("/select")
-async def user_select():
+@app.get("/list")
+async def user_list():
     await database.connect()
-    query = """select * from user_api order by id"""
+    query = """select * from users_test order by id"""
     results = await database.fetch_all(query=query)
     return results
 
 
-@app.get("/gender-limit/")
+@app.get("/gender/")
 async def user_filter(gender: str, limit: int):
     await database.connect()
-    query = f"""select * from user_api where gender = '{gender}' order by id limit {limit}"""
+    query = f"""select * from users_test where gender = '{gender}' order by id limit {limit}"""
     result =  await database.fetch_all(query=query)
     return result
 
-# @app.get("/gender-limit/{gender}")
-# async def user_filter(param: Usergender):
-#     await database.connect()
-#     query = f"""select * from user_api where gender = :gender"""
-#     values = [{"gender":param.gender}]
-#     result =  await database.execute_many(query=query,values=values)
-#     return result
 
-
-@app.post("/post/")
+@app.post("/create/")
 async def user_create(param: Useradd):
     await database.connect()
-    query = """insert into user_api (id,name,gender) values (:id, :name,:gender)"""
+    query = """insert into users_test (id,name,gender) values (:id, :name,:gender)"""
     values = [{"id": param.id,"name": param.name, "gender": param.gender}]
     await database.execute_many(query=query, values=values)
-
-
-# @app.patch("/update/")
-# async def user_update(param: Updateuser):
-#     await database.connect()
-#     query = """update  user_api set gender = :gender where name = :name"""
-#     values = [{"gender": param.gender,"name": param.name}]
-#     await database.execute_many(query=query, values=values)
-#
-# @app.delete("/delete/")
-# async def user_delete(param: Deleteuser):
-#     await database.connect()
-#     query = """delete from  user_api  where name = :name"""
-#     values = [{"name": param.name}]
-#     await database.execute_many(query=query, values=values)
--- database.py
+++ database.py
@@ -7,21 +7,21 @@ async def create():
     database = Database('postgresql://postgres:9785@localhost:5432/postgres')
     try:
         await database.connect()
-        print('Connected to Database')
-        create = """create table user_api (id int, name varchar(10), gender varchar(10))"""
-        print('Created Table user Successfully')
+        print('Соединение с БД установлено')
+        create = """create table users_test (id int, name varchar(10), gender varchar(10))"""
+        print('Таблица users_test создана успешно')
         await database.execute(query=create)
-        insert = """insert into user_api (id,name,gender)
-                     values
-                    (1,'Alex','Male'),
-                    (2,'Ekaterina','Female'),
-                    (3,'Oleg','Male'),
-                    (4,'Anna','Female')"""
+        insert = """insert into users_test (id, name, gender) values
+                    (1,'Александр','Мужской'),
+                    (2,'Константин','Мужской'),
+                    (3,'Светлана','Женский'),
+                    (4,'Алевтина','Женский'),
+                    (5,'Всеволод','Мужской')"""
         await database.execute(query=insert)
         await database.disconnect()
-        print('Disconnecting from Database')
+        print('Соединение с БД разорвано')
 
     except:
-        print('Connection to Database Failed')
+        print('Ошибка подключения к БД')
 
 asyncio.run(create())
